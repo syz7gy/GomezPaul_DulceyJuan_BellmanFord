3eed605d4d9e2bdf5977cf8e41189e3c842dbe5e
MpGomezg <aome.mggm@gmail.com>
2023-10-20 21:55:01 -0500

    cambios a ventana

--- src/co/edu/unbosque/view/ConnectionsMenu.java
+++ src/co/edu/unbosque/view/ConnectionsMenu.java
@@ -12,13 +12,14 @@ import javax.swing.JTextField;
 public class ConnectionsMenu extends JFrame {
 	private JPanel panel;
 	private JLabel mainTitle, description, source, destiny, weight, connectionsLeft;
-	private JTextField sourceNode, destinyNode, weightNode;
-	private JButton submitButton;
+	private JTextField sourceNode, destinyNode, weightNode, initialNode;
+	private JButton submitButton, addButton;
 
 	public JPanel getPanel() {
 		return panel;
 	}
 
+	
 	public void setPanel(JPanel panel) {
 		this.panel = panel;
 	}
@@ -26,7 +27,6 @@ public class ConnectionsMenu extends JFrame {
 	public JLabel getMainTitle() {
 		return mainTitle;
 	}
-	
 
 	public void setMainTitle(JLabel mainTitle) {
 		this.mainTitle = mainTitle;
@@ -104,57 +104,79 @@ public class ConnectionsMenu extends JFrame {
 		this.submitButton = submitButton;
 	}
 
+	public JTextField getInitialNode() {
+		return initialNode;
+	}
+
+	public void setInitialNode(JTextField initialNode) {
+		this.initialNode = initialNode;
+	}
+
+	public JButton getAddButton() {
+		return addButton;
+	}
+
+	public void setAddButton(JButton addButton) {
+		this.addButton = addButton;
+	}
+
 	public ConnectionsMenu() {
 		setTitle("Secondary Menu");
 		setBounds(600, 200, 2500, 900);
-		this.setSize(720, 380);
+		this.setSize(720, 400);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		panel = new JPanel();
 		panel.setLayout(null);
 		panel.setBackground(new Color(0, 234, 0));
 		panel.setBounds(0, 0, 1088, 760);
-		
+
 		mainTitle = new JLabel("Connections");
 		mainTitle.setFont(new Font("Helvetica", Font.BOLD, 26));
-		mainTitle.setBounds(278, 57, 170, 36);
-		
+		mainTitle.setBounds(287, 34, 170, 36);
+
 		description = new JLabel("Enter the connection of the nodes:");
 		description.setFont(new Font("Helvetica", Font.PLAIN, 14));
-		description.setBounds(253, 106, 224, 20);
-		
+		description.setBounds(253, 77, 224, 20);
+
 		source = new JLabel("Source node:");
 		source.setFont(new Font("Helvetica", Font.PLAIN, 14));
-		source.setBounds(118, 148, 100, 20);
-		
+		source.setBounds(118, 122, 100, 20);
+
 		destiny = new JLabel("Destiny node:");
 		destiny.setFont(new Font("Helvetica", Font.PLAIN, 14));
-		destiny.setBounds(316, 148, 88, 20);
-		
+		destiny.setBounds(316, 122, 88, 20);
+
 		weight = new JLabel("Weight:");
 		weight.setFont(new Font("Helvetica", Font.PLAIN, 14));
-		weight.setBounds(521, 148, 49, 20);
-		
+		weight.setBounds(521, 122, 49, 20);
+
 		sourceNode = new JTextField();
-		sourceNode.setBounds(99, 168, 124, 31);
-		
+		sourceNode.setBounds(99, 150, 124, 31);
+
 		destinyNode = new JTextField();
-		destinyNode.setBounds(295, 168, 124, 31);
-		
+		destinyNode.setBounds(295, 150, 124, 31);
+
 		weightNode = new JTextField();
-		weightNode.setBounds(484, 168, 124, 31);
-		
+		weightNode.setBounds(484, 150, 124, 31);
+
 		submitButton = new JButton();
-		submitButton.setBounds(295, 231, 120, 32);
-		
-		connectionsLeft = new JLabel("Number of connections left: ");
+		submitButton.setBounds(394, 314, 120, 32);
+
+		addButton = new JButton();
+		addButton.setBounds(300, 202, 120, 32);
+
+		connectionsLeft = new JLabel("Enter the start node from which the Bellman-Ford system will start:");
 		connectionsLeft.setFont(new Font("Helvetica", Font.PLAIN, 14));
-		connectionsLeft.setBounds(268, 281, 178, 22);
-		
-		
-		
+		connectionsLeft.setBounds(151, 269, 430, 18);
+
+		initialNode = new JTextField();
+		initialNode.setBounds(206, 314, 124, 31);
+
+		panel.add(initialNode);
+		panel.add(addButton);
 		panel.add(mainTitle);
 		panel.add(description);
 		panel.add(source);
